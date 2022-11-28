e23dfd9096ebe6cdcf833ce701c7fba5f8467be0
Nico1998400 <Nico1998@live.se>
2022-11-28 17:06:18 +0100

    Cart klar vi ses

--- screens/ShoppingCartScreen.js
+++ screens/ShoppingCartScreen.js
@@ -1,16 +1,31 @@
-import React, { useContext } from 'react';
-import { View, Text } from 'react-native';
+import React, { useContext, useState, useEffect  } from 'react';
+import { View, Text} from 'react-native';
 import { CartContext } from '../CartContext';
-
-const {items, getItemsCount, getTotalPrice} = useContext(CartContext);
 
 const ShoppingCartScreen = () => {
 
+  const {items, getItemsCount, getTotalPrice} = useContext(CartContext);
+
+    let [total, setTotal] = useState(0);
+
+    useEffect(() => {
+      setTotal(getTotalPrice());
+    });
+
+
   return (
     <View>
-        <Text>Knas</Text>
+      {items.map((product) => (
+        <View>
+        <Text>ProductName: {product.product.productName}</Text>
+        <Text>ProductPrice: {product.product.price}</Text>
+        <Text>Quiantiy: {product.qty}</Text>
+        </View>
+      ))}
+      <Text>{total}</Text>
     </View>
-  )
+);
 }
+
 
 export default ShoppingCartScreen;
